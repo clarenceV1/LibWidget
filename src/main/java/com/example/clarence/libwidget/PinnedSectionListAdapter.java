34ba0bcbd2234fe40869846674250f4667845be1
caishuxing <caishuxing@linggan.com>
2016-08-31 09:59:48 +0800

    添加pinnedlistview控件

---
+++ src/main/java/com/example/clarence/libwidget/PinnedSectionListAdapter.java
@@ -0,0 +1,13 @@
+package com.example.clarence.libwidget;
+
+/**
+ * Created by clarence on 16/8/30.
+ */
+
+import android.widget.ListAdapter;
+
+/** List adapter to be implemented for being used with PinnedSectionListView adapter. */
+public  interface PinnedSectionListAdapter extends ListAdapter {
+    /** This method shall return 'true' if views of given type has to be pinned. */
+    boolean isItemViewTypePinned(int viewType);
+}
